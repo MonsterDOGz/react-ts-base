adef994898a11d58bf557c3dac731782b60c9e3b
Your Name <zhoujie@zhoujiedeiMac.local>
2020-12-11 18:21:33 +0800

    引入组件库

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import React from 'react';
 import './App.css';
 import LikeButton from './components/LikeButton';
 import Positions from './components/Positions';
+import 'vikingship/dist/index.css';
+import { Button, Menu } from 'vikingship';
+import 'antd/dist/antd.css';
+import { Alert } from 'antd';
+// import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 
 function App() {
+  const onClose = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log(e, 'I was closed.');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -22,6 +31,21 @@ function App() {
         </a> */}
         <LikeButton />
         <Positions />
+        <Button btnType="primary" disabled={false} size="sm">
+          按钮
+        </Button>
+        <Alert message="Error Text" description="" type="error" closable onClose={onClose} />
+        <Menu>
+          <Menu.Item>active</Menu.Item>
+          <Menu.Item disabled>disabled</Menu.Item>
+          <Menu.Item>xyz</Menu.Item>
+          <Menu.SubMenu title="dropdown">
+            <Menu.Item>drop1</Menu.Item>
+          </Menu.SubMenu>
+          <Menu.SubMenu title="opened">
+            <Menu.Item>opened1</Menu.Item>
+          </Menu.SubMenu>
+        </Menu>
       </header>
     </div>
   );
